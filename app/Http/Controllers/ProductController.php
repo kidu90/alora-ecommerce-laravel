142182a6dbae111212e796e7ee67c7eb7456471c
masha <mashakidu3@gmail.com>
2025-04-16 20:28:38 +0530

    Added update functionality for products: created update methods in ProductController ; defined API routes for updating product and category.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -72,4 +72,35 @@ class ProductController extends Controller
 
         return response()->json(['message' => 'Product deleted successfully']);
     }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'sometimes|required|string|max:255',
+            'description' => 'sometimes|nullable|string',
+            'price' => 'sometimes|required|numeric|min:0',
+            'stock' => 'sometimes|required|integer|min:0',
+            'ingredients' => 'sometimes|nullable|string',
+            'usage_tips' => 'sometimes|nullable|string',
+            'image' => 'sometimes|nullable|url',
+            'category_id' => 'sometimes|required|exists:categories,id'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $product->update($request->all());
+
+        return response()->json([
+            'message' => 'Product updated successfully!',
+            'product' => $product
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,11 @@ Route::post('/products', [ProductController::class, 'store']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
 
 
 Route::post('/categories', [CategoryController::class, 'store']);
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/categories/{id}', [CategoryController::class, 'show']);
 Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+Route::put('/categories/{id}', [CategoryController::class, 'update']);
